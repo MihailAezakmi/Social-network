dffa876f7cfaa903198bd9932275585c55fb12dc
ivancotx <ivancortx@gmail.com>
2022-11-11 21:45:09 +0200

    NavLink; activeLink; modified Navlink.jsx

--- way_of_the_samurai/src/Content/Navbar/Navbar.jsx
+++ way_of_the_samurai/src/Content/Navbar/Navbar.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import s from './Navbar.module.css'
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
-  return (
-      <div className={s.Navbar}>
-         <div className={s.Navbar_item}><a href="/Profile">Profile</a></div>
-         <div className={s.Navbar_item}><a href="/Dialogs">Message</a></div>
-         <div className={s.Navbar_item}><a href="/News">News</a></div>
-         <div className={s.Navbar_item}><a href="/Music">Music</a></div>
-         <div className={s.Navbar_item}><a href="/Setting">Settings</a></div>
-      </div>
-  )
+    return (
+        <div className={s.Navbar}>
+            <div className={s.Navbar_item}><NavLink to="/Profile" className={({isActive}) =>
+                isActive ? s.active : undefined}>Profile</NavLink></div>
+            <div className={s.Navbar_item}><NavLink to="/Dialogs" className={({isActive}) =>
+                isActive ? s.active : undefined}>Message</NavLink></div>
+            <div className={s.Navbar_item}><NavLink to="/News" className={({isActive}) =>
+                isActive ? s.active : undefined}>News</NavLink></div>
+            <div className={s.Navbar_item}><NavLink to="/Music" className={({isActive}) =>
+                isActive ? s.active : undefined}>Music</NavLink></div>
+            <div className={s.Navbar_item}><NavLink to="/Setting" className={({isActive}) =>
+                isActive ? s.active : undefined}>Settings</NavLink></div>
+        </div>
+    )
 }
 
 export default Navbar;
