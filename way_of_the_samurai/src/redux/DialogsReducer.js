055c52cc80372e013fa85791206a7ce3db3e0bad
ivancotx <ivancortx@gmail.com>
2022-12-09 23:35:02 +0200

    add switch in DialogsReducer.js,ProfileReducer.js

--- way_of_the_samurai/src/redux/DialogsReducer.js
+++ way_of_the_samurai/src/redux/DialogsReducer.js
@@ -2,15 +2,18 @@ const SEND_MESSAGE = "SEND_MESSAGE";
 const UPDATE_NEW_MESSAGE_TEXT = "UPDATE_NEW_MESSAGE_TEXT";
 
 const DialogsReducer = (state, action) => {
-    if (action.type === SEND_MESSAGE) {
-        let body = state.newMessageBody
-        state.newMessageBody = ""
-        state.MessageUserData.push({id: "5", message: body});
-    } else if (action.type === UPDATE_NEW_MESSAGE_TEXT) {
-        state.newMessageBody = action.body
+    switch (action.type) {
+        case SEND_MESSAGE:
+            let body = state.newMessageBody
+            state.newMessageBody = ""
+            state.MessageUserData.push({id: "5", message: body});
+            return state
+        case UPDATE_NEW_MESSAGE_TEXT:
+            state.newMessageBody = action.body
+            return state
+        default:
+            return state
     }
-
-    return state;
 }
 
 export const sendMessageCreator = () => ({type: SEND_MESSAGE})
--- way_of_the_samurai/src/redux/ProfileReducer.js
+++ way_of_the_samurai/src/redux/ProfileReducer.js
@@ -2,14 +2,18 @@ const UPDATE_NEW_POST_TEXT = "UPDATE_NEW_POST_TEXT";
 const ADD_POST = "ADD_POST";
 
 const ProfileReducer = (state, action) => {
-    if (action.type === ADD_POST) {
-        let newPost = state.newPostText;
-        state.newPostText = ""
-        state.postData.push({id: "", post: newPost, like: "15"});
-    } else if (action.type === UPDATE_NEW_POST_TEXT) {
-        state.newPostText = action.newText
+    switch (action.type) {
+        case ADD_POST:
+            let newPost = state.newPostText;
+            state.newPostText = ""
+            state.postData.push({id: "", post: newPost, like: "15"});
+            return state
+        case UPDATE_NEW_POST_TEXT:
+            state.newPostText = action.newText
+            return state
+        default:
+            return state
     }
-    return state;
 }
 
 export const addPostActionCreator = () => ({type: ADD_POST})
