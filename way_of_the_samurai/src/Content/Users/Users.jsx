ec46a22e7db17daabd6bb42147f11e647a1fde9e
ivancotx <ivancortx@gmail.com>
2022-12-23 20:59:32 +0200

    about side effect, add button in components Users.jsx

--- way_of_the_samurai/src/Content/Users/Users.jsx
+++ way_of_the_samurai/src/Content/Users/Users.jsx
@@ -5,19 +5,20 @@ import style from "./Users.module.css"
 import axios from "axios";
 
 
-
 const Users = (props) => {
-    if (props.users.length === 0){
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => props.setUsers(response.data.items))
 
-
+    let getUsers = () => {
+        if (props.users.length === 0)
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => props.setUsers(response.data.items))
     }
+
 
     let user = props.users.map(u => (<User u={u} follow={props.follow} unfollow={props.unfollow}/>))
 
     return <div className={style.wrapper}>
         <div className={style.text}>Users</div>
         <div className={style.users}>{user}</div>
+        <button onClick={getUsers}>Get users</button>
 
     </div>
 
