5134ea6457d71b7f49d13d8c84f57a1f85b42390
ivancotx <ivancortx@gmail.com>
2022-12-26 19:31:27 +0200

    add method componentDidMount in Users.jsx component

--- way_of_the_samurai/src/Content/Users/Users.jsx
+++ way_of_the_samurai/src/Content/Users/Users.jsx
@@ -4,12 +4,11 @@ import style from "./Users.module.css"
 import axios from "axios";
 
 class Users extends React.Component {
-    constructor(props) {
-        super(props);
+    componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => this.props.setUsers(response.data.items))
     }
-
     user = this.props.users.map(u => (<User u={u} follow={this.props.follow} unfollow={this.props.unfollow}/>))
+
 
     render() {
 
